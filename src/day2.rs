c05ceeb1bf4a33277507ad959a6a67ca36882f8a
WalrusWalrus <walruscodes@users.noreply.github.com>
2020-12-03 13:01:36 -0800

    day 2: part 2 works

--- src/day2.rs
+++ src/day2.rs
@@ -1,8 +1,8 @@
 // Represents one parsed line from input file.
 #[derive(Debug)]
 struct Line {
-    len_from: u32,
-    len_to: u32,
+    num1: u32,
+    num2: u32,
     ch: char,
     password: String,
 }
@@ -20,20 +20,18 @@ impl Line {
         let password = items.next()?; // fgfff
 
         let mut lens_split = lens.split('-').map(|x| x.parse::<u32>());
-        let len_from = lens_split.next()?.ok()?;
-        let len_to = lens_split.next()?.ok()?;
+        let num1 = lens_split.next()?.ok()?;
+        let num2 = lens_split.next()?.ok()?;
 
         Some(Line {
-            len_from,
-            len_to,
+            num1,
+            num2,
             ch: ch.chars().next()?,
             password: String::from(password),
         })
     }
 
-    // Checks whether the password in Line matches the pattern.
-    //
-    // 3-5 f: fgfff
+    // Checks whether the password in Line matches the pattern for the first half of the puzzle.
     fn validate(self: &Self) -> bool {
         let mut cnt = 0;
         for c in self.password.chars() {
@@ -41,7 +39,18 @@ impl Line {
                 cnt += 1;
             }
         }
-        (cnt >= self.len_from) && (cnt <= self.len_to)
+        (cnt >= self.num1) && (cnt <= self.num2)
+    }
+
+    // Checks whether the password in Line matches the pattern for the first half of the puzzle.
+    fn validate2(self: &Self) -> bool {
+        let ch1 = self.password.chars().nth(self.num1 as usize - 1).unwrap();
+        let ch2 = self.password.chars().nth(self.num2 as usize - 1).unwrap();
+
+        let v1 = ch1 == self.ch;
+        let v2 = ch2 == self.ch;
+
+        (v1 && !v2) || (!v1 && v2)
     }
 }
 
@@ -52,8 +61,8 @@ mod tests {
     #[test]
     fn parse_line_works() {
         let line = Line::parse("3-5 f: fgfff").unwrap();
-        assert_eq!(line.len_from, 3);
-        assert_eq!(line.len_to, 5);
+        assert_eq!(line.num1, 3);
+        assert_eq!(line.num2, 5);
         assert_eq!(line.ch, 'f');
         assert_eq!(line.password, "fgfff");
     }
@@ -66,6 +75,13 @@ mod tests {
     #[test]
     fn validate_line_bad() {
         assert!(!Line::parse("1-3 b: cdefg").unwrap().validate());
+    }
+
+    #[test]
+    fn validate_line2() {
+        assert!(!Line::parse("1-3 b: cdefg").unwrap().validate2());
+        assert!(Line::parse("1-3 b: bdefg").unwrap().validate2());
+        assert!(!Line::parse("1-3 b: bdbfg").unwrap().validate2());
     }
 }
 
@@ -82,5 +98,10 @@ fn main() {
         .iter()
         .fold(0, |cnt, l| if l.validate() { cnt + 1 } else { cnt });
 
-    println!("valid passwords: {}", valid_passwords);
+    let valid_passwords2 = items
+        .iter()
+        .fold(0, |cnt, l| if l.validate2() { cnt + 1 } else { cnt });
+
+    println!("valid passwords method 1: {}", valid_passwords);
+    println!("valid passwords method 2: {}", valid_passwords2);
 }
