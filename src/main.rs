80da01cd97262c8953268f94c30f2b6335835061
WalrusWalrus <walruscodes@users.noreply.github.com>
2020-12-02 19:25:59 -0800

    day 2: second half solved as well, now with a HashSet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::collections::HashSet;
+
+// Day 1, part 1:
+//
+// Given a set of numbers, finds 2 numbers a, b, such that a+b==2020, and
+// returns a*b.
+fn find2(items: &HashSet<i32>) -> i32 {
+    for a in items.iter() {
+        let b = 2020 - a;
+        if items.contains(&b) {
+            println!("{} {}", a, b);
+            return a * b;
+        }
+    }
+    panic!("not found!");
+}
+
+// Day 1, part 2:
+//
+// Given a set of numbers, finds 3 numbers a, b, c, such that a+b+c==2020, and
+// returns a*b*c.
+fn find3(items: &HashSet<i32>) -> i32 {
+    for a in items.iter() {
+        for b in items.iter() {
+            let c = 2020 - a - b;
+            if items.contains(&c) {
+                println!("{} {} {}", a, b, c);
+                return a * b * c;
+            }
+        }
+    }
+    panic!("not found!");
+}
+
 fn main() {
     let input = std::env::args().nth(1).expect("no arg");
     let contents = std::fs::read_to_string(input).expect("read failed");
@@ -5,19 +39,8 @@ fn main() {
     let items = contents
         .lines()
         .map(|s| s.parse::<i32>().expect("failed to parse"))
-        .collect::<Vec<i32>>();
-    for i in 0..items.len() {
-        for j in (i + 1)..items.len() {
-            if (items[i] + items[j]) == 2020 {
-                println!(
-                    "{} + {} == 2020 {} * {} = {}",
-                    items[i],
-                    items[j],
-                    items[i],
-                    items[j],
-                    items[i] * items[j]
-                );
-            }
-        }
-    }
+        .collect::<HashSet<i32>>();
+
+    println!("find 2: {}", find2(&items));
+    println!("find 3: {}", find3(&items));
 }
