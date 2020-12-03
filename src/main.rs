8c8ff39635faff90858a292c0d5f65bfac433ff8
WalrusWalrus <walruscodes@users.noreply.github.com>
2020-12-02 18:50:32 -0800

    day 1: first half solved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::env::args().nth(1).expect("no arg");
+    let contents = std::fs::read_to_string(input).expect("read failed");
+
+    let items = contents
+        .lines()
+        .map(|s| s.parse::<i32>().expect("failed to parse"))
+        .collect::<Vec<i32>>();
+    for i in 0..items.len() {
+        for j in (i + 1)..items.len() {
+            if (items[i] + items[j]) == 2020 {
+                println!(
+                    "{} + {} == 2020 {} * {} = {}",
+                    items[i],
+                    items[j],
+                    items[i],
+                    items[j],
+                    items[i] * items[j]
+                );
+            }
+        }
+    }
 }
